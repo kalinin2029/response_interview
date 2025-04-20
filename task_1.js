486f69e35fbeef3c5a353a516ba1ffdf051bb5cf
kalinin2029 <kalinin2029@yandex.ru>
2025-04-20 23:09:59 +0300

    task_1.js push

---
+++ task_1.js
@@ -0,0 +1,13 @@
+Promise.resolve(10) //
+.then(console.log.bind(null, 'A:')) //В первый раз выведет А: 10.
+//.then((value) => console.log('A:', value)) // аналогичный способ, как предложил DeepSeek
+.then(res => !res
+    ? new Error('Got error')        //Так как, в первом then мы в методе bind указали (null), вернется пустое значение и выполниться Error.
+    :res
+)
+.then(res => console.info('B:', res))
+.catch(res => console.error('D:', res)) // Не выполниться, так как промис не был отклонен.
+
+//В консоле у нас будет.
+//A: 10
+//B: Error: Got error.
