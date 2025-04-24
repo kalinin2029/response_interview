489d95ba0792fcf958032fe97460f9a267f45aa0
kalinin2029 <kalinin2029@yandex.ru>
2025-04-24 17:59:47 +0300

    edit sql

--- task_6_7.sql
+++ task_6_7.sql
@@ -8,22 +8,13 @@ from users as u
 left join stat as s on s.id_user=u.id -- некоторые пишут u.id=s.id_user, разницы нет. 
 group by u.id, u,name
 
---второй вариант с with
-with user_rank (
-    select
-         u.id
-        ,u.name
-        ,s.rank 
-    from users as u
-    left join stat as s on s.id_user=u.id 
-    group by u.id, u,name
-)
-
+--второй вариант
 select 
-     id as 'id of user'
-    ,name as 'Name of user'
-    ,rank as 'Current rank'
-    ,min(rank) as 'Min rank'
-    ,max(rank) as 'Max rank'
-from user_rank
-group by id, name, rank 
+    u.id as "ID_User",
+    u.name as "Name_User",
+    coalesce(s.rank,0)as "Rank",
+    min(coalesce(s.rank,0)) over(partition by u.id) as "Min_Rank",
+    max(coalesce(s.rank,0)) over(partition by u.id) as "Max_Rank",
+    round(avg(coalesce(s.rank,0)) over(partition by u.id)) as "Avg_rank"
+from public."user" as u
+left join public."stat" as s on s.id_user=u.id
